e344c58c227782235efdb59819a7032fd3154f73
Sidieu Guy <sidieuguy2001@yahoo.fr>
2024-09-17 07:33:51 +0200

    Added project Dialecttranslator

---
+++ DialectTranslator/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace DialectTranslator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 200);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(19, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(445, 230);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(437, 204);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Français";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(437, 204);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Nihep";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(0, 0);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(437, 208);
+            this.textBox4.TabIndex = 0;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Location = new System.Drawing.Point(470, 41);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(445, 230);
+            this.tabControl2.TabIndex = 2;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(437, 204);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Nihep";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(3, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(434, 200);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.textBox3);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(437, 204);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "Ewondo";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(0, 0);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(434, 204);
+            this.textBox3.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::DialectTranslator.Properties.Resources.listen;
+            this.button2.Location = new System.Drawing.Point(590, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 55);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::DialectTranslator.Properties.Resources.swap;
+            this.button1.Location = new System.Drawing.Point(104, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 55);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 455);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "DialecteTraducteur";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ DialectTranslator/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Media;
+using System.Text.RegularExpressions;
+
+namespace DialectTranslator
+{
+    public partial class Form1 : Form
+    {
+        int tabcomNr = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Fr_Ni1 = "Traduction\\dico1.txt"; 
+            var Fr_Ni2 = "Traduction\\dico2.txt";
+            var Fr_Ew1 = "Traduction\\Fr_Ew1.txt";
+            var Fr_Ew2 = "Traduction\\Fr_Ew2.txt";
+
+            int tabNr1 = tabControl1.SelectedIndex;
+            int tabNr2 = tabControl2.SelectedIndex;
+
+            string tabn1 = tabNr1.ToString();
+            string tabn2 = tabNr2.ToString();
+
+            string tabCombineNr = string.Concat(tabn1, tabn2);
+            tabcomNr = Int32.Parse(tabCombineNr);
+
+            switch (tabcomNr)
+            {
+                case (0):
+                    traduction(Fr_Ni1, Fr_Ni2);
+                    break;
+                case (1):
+                    traduction(Fr_Ew1, Fr_Ew2);
+                    break;
+
+
+                    //default:
+                    //    Console.WriteLine($"Measured value is {measurement}.");
+                    //    break;
+            }
+
+        }
+
+
+
+
+
+
+        //SpeechSynthesizer reader = new SpeechSynthesizer();
+
+        private void traduction(string PathName1, string PathName2 = "") 
+        {
+            try
+            {
+                //var pathlocation = "Traduction\\dico1.txt"; //C:\\Data\\disco.csv
+                //var pathlocation1 = "Traduction\\dico2.txt";
+                string value = "";
+                string value1 = "";
+                string value2 = "";
+                bool TextOK1 = true;
+
+
+                //string path = Path.Combine(pathlocation);
+
+                FileInfo file = new FileInfo(PathName1);
+                FileInfo file1 = new FileInfo(PathName2);
+
+                if ((file.Exists) || (file1.Exists))
+                {
+                    var lines = File.ReadAllLines(PathName1);
+                    var lines1 = File.ReadAllLines(PathName2);
+                    string subs2 = textBox1.Text.TrimEnd(' ');
+
+                    for (int c = 0; c < lines1.Length; c++)
+                    {
+                        string[] subs4 = lines1[c].Split(',');
+
+                        if (subs2.Equals(subs4[0].ToString(), StringComparison.OrdinalIgnoreCase))
+                        {
+                            switch (tabcomNr)
+                            {
+                                case (0):
+                                    textBox2.Text = subs4[1].ToString();
+                                    break;
+                                case (1):
+                                    textBox3.Text = subs4[1].ToString();
+                                    break;
+                                default:
+                                    break;
+                            }
+                            TextOK1 = false;
+                        }
+
+
+                    }
+
+                    if (TextOK1)
+                    {
+                        string subs5 = textBox1.Text.TrimEnd(' ');
+                        string[] subs3 = subs5.Split(' ');
+                        for (int i = 0; i < subs3.Length; i++)
+                        {
+                            for (int j = 0; j < lines.Length; j++)
+                            {
+                                string[] subs = lines[j].Split(',');
+
+                                if (subs3[i].Equals(subs[0], StringComparison.OrdinalIgnoreCase))
+                                {
+                                    value += string.Concat(subs[0], " ");
+                                    value1 += string.Concat(subs[1], " ");
+                                    value2 = value.TrimEnd(' ');
+                                    break;
+                                }
+
+                            }
+                        }
+                        switch (tabcomNr)
+                        {
+                            case (0):
+                                textBox2.Text = value1.ToString();
+                                break;
+                            case (1):
+                                textBox3.Text = value1.ToString();
+                                break;
+                            default:
+                                break;
+                        }
+                        
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+                MessageBox.Show("Please enter some text first!");
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (tabcomNr)
+                {
+                    case (0):
+                        playsound("Soundaufnahmen\\Nihep",textBox2.Text);
+                        break;
+                    case (1):
+                        playsound("Soundaufnahmen\\Ewondo",textBox3.Text);
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+
+        }
+
+        private void playsound(string soundPath, string textbox ) 
+        {
+            var folderloca = soundPath;
+            var dir = new DirectoryInfo(folderloca);
+            if (dir.Exists)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string[] subs = files[i].ToString().Split('.');  //trennen nach dem punkt
+                    textbox = textbox.TrimEnd(' ');
+                    if ((textbox != "") || (textBox1.Text != ""))
+                    {
+                        if (textbox.Equals(subs[0], StringComparison.OrdinalIgnoreCase))
+                        {    
+                            var soundloc = Path.Combine(soundPath, files[i].ToString());
+                            SoundPlayer sound = new SoundPlayer(soundloc);
+                            sound.PlaySync();                    
+
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter some text first!");
+                        break;
+                    }
+
+
+                }
+
+
+            }
+        }
+    }
+}
