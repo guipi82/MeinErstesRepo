6c7d26b6e5dd7c9e7911a234720b6b387935728e
Sidieu Guy <sidieuguy2001@yahoo.fr>
2024-09-21 14:52:31 +0200

    check if a employee is on list

---
+++ TestCICDJenkins/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCICDJenkins
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string Firstname { get; set; }
+        public int ID { get; set; }
+
+        public Person( string lastname, string firstname)
+        {
+            LastName = lastname;
+            Firstname = firstname;
+        }
+        public static bool AreYouEmployee1(Person person, List<Person> EmployeeList)
+        {
+            foreach (var item in EmployeeList)
+            {
+                if((item.LastName == person.LastName) &&(item.Firstname == person.Firstname)) 
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public bool AreYouEmployee2(List<Person> EmployeeList)
+        {
+            return  EmployeeList.Any(x => x.LastName == LastName && x.Firstname == Firstname);
+        }
+    }
+}
---
+++ TestCICDJenkins/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace TestCICDJenkins
+{
+    internal class Program
+    {
+               
+        static void Main(string[] args)
+        {
+            List<Person> EmployeeList = new List<Person> { new Person("Tchuimba", "Alphonsine"),
+                                                           new Person("Tchayep Sidieu", "Guy"),
+                                                           new Person("Tchayep Sidieu", "Ange David"),
+                                                           new Person("Nana Tchayep", "Achille"),
+                                                           new Person("Tchanang Tchayep", "Carine"),
+                                                           new Person("Gohres", "Tim Emanuel")
+                                                         };            
+            Person employee1 = new Person("Tchanang Tchayep", "Carine");
+            Stopwatch stopwatch = new Stopwatch();
+            
+            stopwatch.Start();
+            //Console.WriteLine(Person.AreYouEmployee1(employee1, EmployeeList));
+            Person.AreYouEmployee1(employee1, EmployeeList);
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time for the function AreYouEmployee1: {stopwatch.Elapsed.TotalMilliseconds} ms ");
+            stopwatch.Start();
+            //Console.WriteLine(employee1.AreYouEmployee2(EmployeeList));
+            employee1.AreYouEmployee2(EmployeeList);
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time for the function AreYouEmployee2: {stopwatch.Elapsed.TotalMilliseconds} ms ");
+        }
+
+        
+    }
+}
